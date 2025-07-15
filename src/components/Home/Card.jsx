baeedb98e274d4a59ef6619c330d7139fa3d4149
zainkhokhar1 <muhammadzain7113@gmail.com>
2025-07-16 01:15:48 +0500

    Updated the home page

---
+++ src/components/Home/Card.jsx
@@ -0,0 +1,89 @@
+import { View, Image, Text } from 'react-native'
+
+const Card = () => {
+    return (
+        <View className="relative flex-1 mt-12 items-center">
+
+            <View className="bg-[#5655B9] rounded-[10px] w-10/12 h-[165px] relative z-0"
+                style={{
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 4 },
+                    shadowOpacity: 0.05,
+                    shadowRadius: 20,
+                    elevation: 2,
+                }}
+            >
+            </View>
+
+            <View className="bg-[#FF4267] rounded-[10px] w-11/12 h-[165px] z-10 absolute bottom-3"
+                style={{
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 4 },
+                    shadowOpacity: 0.05,
+                    shadowRadius: 20,
+                    elevation: 2,
+                }}
+            >
+            </View>
+
+            <View className="bg-[#1573FF] rounded-[10px] w-full h-[165px] z-10 absolute bottom-6 overflow-hidden"
+                style={{
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 4 },
+                    shadowOpacity: 0.05,
+                    shadowRadius: 20,
+                    elevation: 2,
+                }}
+            >
+                {/* first circle on the right */}
+                <View className="h-[185px] w-[191px] absolute rounded-full -right-[90px] -top-[80px]">
+                    <Image
+                        source={
+                            require('../../../assets/right-circle.png')
+                        }
+                        className="h-full w-full object-contain"
+
+                    />
+                </View>
+
+                {/* second circle on the right */}
+                <View className="h-[340px] w-[350px] absolute rounded-full -left-[140px] -top-[60px]">
+                    <Image
+                        source={
+                            require('../../../assets/left-circle.png')
+                        }
+                        className="h-full w-full object-contain"
+
+                    />
+                </View>
+
+                <Text className="text-white text-2xl relative top-5 left-5">
+                    John Smith
+                </Text>
+
+                <Text className="text-white text-sm font-medium leading-4 relative top-[30px] left-5">
+                    Amazon Platinium
+                </Text>
+
+                <Text className="text-white text-sm leading-6 font-medium relative top-[45px] left-5">
+                    4756 **** **** 9018
+                </Text>
+
+                <Text className="text-white relative top-[52px] left-5 text-xl font-semibold leading-7">
+                    $3.469.52
+                </Text>
+
+                <Image
+                    source={
+                        require('../../../assets/visa.png')
+                    }
+                    className="w-[47px] h-[16px] absolute bottom-6 right-6"
+                />
+
+            </View>
+
+        </View>
+    )
+}
+
+export default Card
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, ScrollView, Image } from 'react-native'
+import { View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Card from '../components/Home/Card';
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View className="bg-[#3629B7] flex-1 pt-10">
+
+            {/* Container for the profile section */}
+            <View className="flex-row items-center px-6 ">
+                {/* profile container */}
+                <View className="h-[50px] w-[50px] rounded-full overflow-hidden bg-white">
+                    <Image
+                        source={
+                            require('../../assets/profile.jpg')
+                        }
+                        className="h-full w-full object-cover"
+                    />
+                </View>
+
+                {/* Welcome Text */}
+                <Text className="pl-4 text-white text-base font-medium leading-6 pr-24">
+                    Hi, Push Puttichai
+                </Text>
+
+                {/* Icon container in the last */}
+                <View className="flex-1 items-center justify-center relative">
+                    {/* icon */}
+                    <Ionicons name="notifications" size={28} color="white" />
+                    <Text className="absolute -top-[4px] right-[14px] w-[16px] h-[16px] text-center text-white  bg-[#FF4267] rounded-full text-[10px] font-medium leading-4">
+                        3
+                    </Text>
+                </View>
+            </View>
+
+            {/* Container to show the main content. */}
+            <ScrollView className="bg-white flex-1 rounded-tl-[30px] rounded-tr-[30px] px-6 mt-6">
+
+                {/* Card to show */}
+                <Card />
+
+            </ScrollView>
+
         </View>
     )
 }
